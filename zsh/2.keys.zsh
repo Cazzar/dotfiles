a592a6bdce3d77f7da064050e8497b5d9c4a6d54
Cayde Dixon <me@cazzar.net>
2015-11-02 17:17:42 +1100

    Update the key codes so they work via ssh

--- zsh/2.keys.zsh
+++ zsh/2.keys.zsh
@@ -1,6 +1,3 @@
-#!/bin/zsh
-
-bindkey -e
 
 KEY_FILE="${ZDOTDIR:-$HOME}/.zkbd/${TMUX_TERM:-$TERM}-$VENDOR-$OSTYPE"
 
@@ -28,12 +25,12 @@ else
   echo
 
   typeset -g -A key
-  key[Home]="\e[1~"
-  key[End]="\e[4~"
-  key[PageUp]="\e[5~"
-  key[PageDown]="\e[6~"
-  key[Insert]="\e[2~"
-  key[Delete]="\e[3~"
+  key[Home]="^[[1~"
+  key[End]="^[[4~"
+  key[PageUp]="^[[5~"
+  key[PageDown]="^[[6~"
+  key[Insert]="^[[2~"
+  key[Delete]="^[[3~"
 fi
 
 [[ -n "${key[Home]}"     ]]  && bindkey  "${key[Home]}"     beginning-of-line
@@ -48,18 +45,18 @@ fi
 [[ -n "${key[PageDown]}" ]]  && bindkey  "${key[PageDown]}" end-of-history
 
 # Alt-Right
-bindkey "\e[5C" forward-word
-bindkey "\e\e[C" forward-word
-bindkey "\ef" forward-word
-bindkey "\eOc" emacs-forward-word
+bindkey "^[[1;3C" forward-word
+bindkey "^[^[[1;3C" forward-word
+bindkey "^[f" forward-word
+bindkey "^[Oc" emacs-forward-word
 
 # Alt-Left
-bindkey "\e[5D" backward-word
-bindkey "\e\e[D" backward-word
-bindkey "\eb" backward-word
-bindkey "\eOd" emacs-backward-word
+bindkey "^[[1;3D" backward-word
+bindkey "^[^[[1;3D" backward-word
+bindkey "^[b" backward-word
+bindkey "^[Od" emacs-backward-word
 
 # Shift-Tab
-bindkey "\e[Z" reverse-menu-complete
+bindkey "^[[Z" reverse-menu-complete
 
 unset KEY_FILE
